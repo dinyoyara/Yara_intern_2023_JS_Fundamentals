931a5b6a42df83903662904f59baa2c7bb874e8c
dinyoyara <dinyo.dimitrov@yara.com>
2023-02-09 10:51:23 +0200

    Implement reset btn and fix functionalitiy bugs.

--- index.js
+++ index.js
@@ -8,33 +8,75 @@ const inputHours = document.querySelector('.input-hours');
 const inputMinutes = document.querySelector('.input-minutes');
 const inputSec = document.querySelector('.input-seconds');
 
-const btn = document.querySelector('.btn');
+const btnStart = document.querySelector('.btn-start');
+const btnReset = document.querySelector('.btn-reset');
 
-btn.addEventListener('click', function () {
-    const inputDate = new Date(
+const resultConteiner = document.querySelector('.result-boxes-container');
+
+const timePeriod = {
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0
+};
+let setIntervalNumber;
+
+const getInputDate = () =>
+    new Date(
         inputYear.value,
         inputMonth.value - 1,
         inputDay.value,
         inputHours.value,
         inputMinutes.value,
         inputSec.value
-        ).getTime();
+    ).getTime();
 
-        counting(inputDate);
-    });
+const getPeriod = (inputDate) => {
+    const dateNow = Date.now();
+    const period = inputDate - dateNow;
+    return period;
+};
 
-const counting = (inputDate) => setInterval(() => {
-        const dateNow = Date.now();
-        const period = inputDate - dateNow;
+const getTimePeriod = (inputDate) => {
+    const period = getPeriod(inputDate);
 
-    let days = Math.floor(period / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((period % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((period % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((period % (1000 * 60)) / 1000);
+    timePeriod.days = Math.floor(period / (1000 * 60 * 60 * 24));
+    timePeriod.hours = Math.floor((period % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    timePeriod.minutes = Math.floor((period % (1000 * 60 * 60)) / (1000 * 60));
+    timePeriod.seconds = Math.floor((period % (1000 * 60)) / 1000);
+
+    return timePeriod;
+};
+const showTimePeriod = (inputDate) => {
+    const { days, hours, minutes, seconds } = getTimePeriod(inputDate);
 
     document.querySelector('.result-days').textContent = days;
     document.querySelector('.result-hours').textContent = hours;
     document.querySelector('.result-minutes').textContent = minutes;
     document.querySelector('.result-sec').textContent = seconds;
-    
-}, 1000);
+};
+
+const counting = (inputDate) => setInterval(showTimePeriod.bind(null, inputDate), 1000);
+
+const changeVisibility = (element) => {
+    element.classList.toggle('hidden');
+};
+
+const switchButtons = () => {
+    changeVisibility(btnStart);
+    changeVisibility(btnReset);
+};
+
+btnStart.addEventListener('click', function () {
+    const inputDate = getInputDate();
+    showTimePeriod(inputDate);
+    setIntervalNumber = counting(inputDate);
+    changeVisibility(resultConteiner);
+    switchButtons();
+});
+
+btnReset.addEventListener('click', function () {
+    changeVisibility(resultConteiner);
+    clearInterval(setIntervalNumber);
+    switchButtons();
+});
